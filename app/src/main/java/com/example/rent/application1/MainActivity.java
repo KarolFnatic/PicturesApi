a72bef1f09de962064de8e235228061f1fb85463
michal.papuzynski <michal.papuzynski@gmail.com>
2017-08-17 20:46:56 +0200

    Added Picture model Added BaseRetrofit retrofit Added PicturesApi service Added Butterknife, Picasso and Retrofit in gradle

--- app/src/main/java/com/example/rent/application1/MainActivity.java
+++ app/src/main/java/com/example/rent/application1/MainActivity.java
@@ -3,11 +3,46 @@ package com.example.rent.application1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.rent.application1.service.PicturesApi;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import butterknife.ButterKnife;
+import io.reactivex.annotations.NonNull;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String BASE_URL = "https://unsplash.it";
+    private Retrofit retrofit;
+    private PicturesApi picturesApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        prepareRetrofit();
+
+    }
+
+    private void prepareRetrofit() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(getGson()))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+
+        picturesApi = retrofit.create(PicturesApi.class);
+    }
+
+    @NonNull
+    private Gson getGson() {
+        return new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .setLenient()
+                .create();
     }
 }
