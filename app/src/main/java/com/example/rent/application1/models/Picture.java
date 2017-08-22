f5da6d638386972d902908b2dc5972ec57edcc5a
Karol Grabowski <grabowski-karol@wp.pl>
2017-08-22 18:00:16 +0200

    Implement Parcelable

--- app/src/main/java/com/example/rent/application1/models/Picture.java
+++ app/src/main/java/com/example/rent/application1/models/Picture.java
@@ -1,16 +1,37 @@
 package com.example.rent.application1.models;
 
 import android.media.Image;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Picture {
+public class Picture implements Parcelable{
 
-    // TODO: 2017-08-21 Implement parcelable
 
     private String filename;
     private String author;
     private int height;
     private int width;
     private int id;
+
+    protected Picture(Parcel in) {
+        filename = in.readString();
+        author = in.readString();
+        height = in.readInt();
+        width = in.readInt();
+        id = in.readInt();
+    }
+
+    public static final Creator<Picture> CREATOR = new Creator<Picture>() {
+        @Override
+        public Picture createFromParcel(Parcel in) {
+            return new Picture(in);
+        }
+
+        @Override
+        public Picture[] newArray(int size) {
+            return new Picture[size];
+        }
+    };
 
     public int getHeight() {
         return height;
@@ -58,5 +79,19 @@ public class Picture {
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(filename);
+        parcel.writeString(author);
+        parcel.writeInt(height);
+        parcel.writeInt(width);
+        parcel.writeInt(id);
     }
 }
